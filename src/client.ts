0fef22b54dbdc0d47cf0cf34ecce35800dabafd4
Remco Goyvaerts <remco.goyvaerts@acagroup.be>
2025-01-07 12:57:06 +0100

    Basic client with basic tests

--- src/client.ts
+++ src/client.ts
@@ -1,20 +1,75 @@
-const getApiClient = (apiKey: string) => {
+import qs from "https://deno.land/x/deno_qs/mod.ts";
+
+export const getApiClient = (apiKey: string) => {
     return new Client(apiKey);
 }
 
-const getClient = () => {
+export const getClient = () => {
     return new Client();
 }
 
 class Client {
     apiKey: string;
+    host: string = 'https://api.pokemontcg.io/v2';
 
     constructor(apiKey?: string) {
         this.apiKey = apiKey || '';
     }
-}
 
-export default {
-    getApiClient,
-    getClient
+    private _getOptions() {
+        const headers: any = {};
+
+        if (this.apiKey) {
+            headers['X-Api-Key'] = this.apiKey;
+        }
+
+        return {
+            headers
+        }
+    }
+
+    private _get(path: string, args: any): Promise<any> {
+        return fetch(`${this.host}/${path}${args && '?' + qs.stringify(args)}`, {
+            ...this._getOptions(),
+            method: 'GET'
+        })
+        .then(response => response.json());
+    }
+
+    private _queryBuilder = (type: string) => ({
+        find: (id: string) => {
+            return fetch(`${this.host}/${type}/${id}`, {
+                ...this._getOptions(),
+                method: 'GET'
+            })
+            .then(response => response.json())
+            .then(json => json.data);
+        },
+        where: (args: any) => this._get(type, args),
+        all: (args: any={}, data: any[]=[]) => {
+            const getAll = (type: string, args: any): Promise<any> => {
+                const page = args.page ? args.page + 1 : 1;
+    
+                return this._get(type, {...args, page})
+                    .then(response => {
+                        data.push(...response.data);
+    
+                        if (!response.totalCount || (response.pageSize * response.page) >= response.totalCount) {
+                            return data;
+                        }
+    
+                        return getAll(type, {...args, page})
+                    })
+                    .catch(error => console.error(error));
+            }
+            return getAll(type, args);
+        }
+    })
+
+    cards = this._queryBuilder('cards');
+    sets = this._queryBuilder('sets');
+    types = this._queryBuilder('types');
+    subtypes = this._queryBuilder('subtypes');
+    rarity = this._queryBuilder('rarity');
+    supertypes = this._queryBuilder('supertypes');
 }
---
+++ test.ts
@@ -0,0 +1,51 @@
+import { assertEquals, assertStringIncludes } from "jsr:@std/assert";
+
+import { getApiClient, getClient } from "./mod.ts";
+
+Deno.test('getClient has no API key', () => {
+    const client = getClient();
+
+    assertEquals(client.apiKey, '');
+});
+
+Deno.test('getApiClient has an API key', () => {
+    const client = getApiClient('1234567890');
+
+    assertEquals(client.apiKey, '1234567890');
+});
+
+Deno.test('find pokemon card', async () => {
+    const client = getClient();
+
+    const card = await client.cards.find('sv3pt5-1');
+
+    assertEquals(card.name, 'Bulbasaur');
+})
+
+Deno.test('find set information', async () => {
+    const client = getClient();
+
+    const set = await client.sets.find('sv3pt5');
+
+    assertEquals(set.name, '151');
+})
+
+Deno.test('find all types', async () => {
+    const client = getClient();
+
+    const types = await client.types.all();
+
+    assertEquals(types.length, 11);
+})
+
+Deno.test('find all pokemon cards with name Rotom', async () => {
+    const client = getClient();
+
+    const cards = await client.cards.all({q: 'name:Rotom'});
+
+    for (const card of cards) {
+        assertStringIncludes(card.name, 'Rotom');
+    }
+
+    assertEquals(cards.length, 45);
+})
